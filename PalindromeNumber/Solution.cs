de2828495663c062a840fc31b89bb8e4c4d2a523
Pablo Rausch <pablorausch@gmail.com>
2020-01-11 12:07:41 -0300

    solution

---
+++ PalindromeNumber/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeNumber
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+
+            if (x < 0)
+                return false;
+
+            if (x == 0)
+                return true;
+
+            if (x % 10 == 0)
+                return false;
+
+            var part = x;
+            var reversed = 0;
+
+            while (part > 0)
+            {
+                reversed = reversed * 10 + part % 10;
+                part /= 10;
+            }
+
+            return reversed == x;
+        }
+    }
+}
